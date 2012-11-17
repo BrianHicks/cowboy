fb516fd6439e1c387b72576a02bc1d3c40d88280
Brian Hicks <brian.hicks@rockfishinteractive.com>
2012-11-16 22:22:54 -0600

    add parse_utc_offset

--- test/timezone_tests.py
+++ test/timezone_tests.py
@@ -1,22 +1,54 @@
 'tests for timezone'
-from datetime import tzinfo
+from datetime import tzinfo, timedelta, datetime
 from unittest import TestCase
 
-from times.timezone import TimeZone
+from times.timezone import parse_utc_offset
 
 
-class MetaTests(TestCase):
-    'test subclasses, special cases in docs for tzinfo'
-    def test_subclass(self):
-        'TimeZone is a subclass of tzinfo'
-        self.assertTrue(issubclass(TimeZone, tzinfo))
+#class MetaTests(TestCase):
+    #'test subclasses, special cases in docs for tzinfo'
+    #def test_subclass(self):
+        #'TimeZone is a subclass of tzinfo'
+        #self.assertTrue(issubclass(TimeZone, tzinfo))
 
-    def test_init_takes_no_arguments(self):
-        '__init__ takes no arguments (so it can be unpickled)'
-        # see http://docs.python.org/2/library/datetime.html#tzinfo-objects
-        try:
-            TimeZone()
-        except Exception as err:
-            self.fail(
-                'TimeZone cannot be instantiated with no arguments. Error: %s' % err
-            )
+    #def test_init_takes_no_arguments(self):
+        #'__init__ takes no arguments (so it can be unpickled)'
+        ## see http://docs.python.org/2/library/datetime.html#tzinfo-objects
+        #try:
+            #TimeZone()
+        #except Exception as err:
+            #self.fail(
+                #'TimeZone cannot be instantiated with no arguments. Error: %s' % err
+            #)
+
+class ParseUtcOffsetTests(TestCase):
+    'parse_utc_offset tests'
+    def test_none(self):
+        self.assertEqual(
+            (None, None, None),
+            parse_utc_offset(None)
+        )
+
+    def test_positive(self):
+        self.assertEqual(
+            ('east', 0, 0),
+            parse_utc_offset('+0000')
+        )
+
+    def test_negative(self):
+        self.assertEqual(
+            ('west', 0, 0),
+            parse_utc_offset('-0000')
+        )
+
+    def test_hours(self):
+        self.assertEqual(
+            ('west', 5, 0),
+            parse_utc_offset('-0500')
+        )
+
+    def test_minutes(self):
+        self.assertEqual(
+            ('west', 0, 30),
+            parse_utc_offset('-0030')
+        )
--- times/timezone.py
+++ times/timezone.py
@@ -1,7 +1,12 @@
 'timezone class'
-from datetime import tzinfo
+from datetime import tzinfo, timedelta
 
-class TimeZone(tzinfo):
-    'timezone object'
-    def __init__(self, offset=0):
-        self.offset = 0
+def parse_utc_offset(offset=None):
+    if offset is None:
+        return (None, None, None)
+
+    return (
+        {'+': 'east', '-': 'west'}[offset[0]],
+        int(offset[1:3]),
+        int(offset[3:5])
+    )
